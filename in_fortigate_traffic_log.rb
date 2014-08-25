70c3f7d749ea7f06a90f3db2bed3afd5514f5255
kongou-ae <y.matsumoto.ae@gmail.com>
2014-08-26 00:11:12 +0900

    トラフィックログのみに限定

--- in_fortigate_traffic_log.rb
+++ in_fortigate_traffic_log.rb
@@ -9,31 +9,34 @@ class FortgateTrafficLog < Fluent::TailInput
 
   # Override 'parse_line(line)' method that returns time and record.
   def parse_line(line)
-    # ログを,で分割して配列に格納する
-    elements = line.split(",")
+    if /.*type=traffic.*/ =~ line
+      # ログを,で分割して配列に格納する
+      elements = line.split(",")
 
-    # dateとtimeを結合して、実際の値を正規表現で抽出する
-    tmp = "#{elements[0]},#{elements[1]}"
-    tmp =~ /.+date=(.+),time=(.+)/
-    tmp_date = $1
+      # dateとtimeを結合して、実際の値を正規表現で抽出する
+      tmp = "#{elements[0]},#{elements[1]}"
+      tmp =~ /.+date=(.+),time=(.+)/
+      tmp_date = $1
 
-    # timeの中に含まれる半角スペースを除去する。$
-    tmp_time = $2.gsub(/(\s)+/,'')
+      # timeの中に含まれる半角スペースを除去する。$
+      tmp_time = $2.gsub(/(\s)+/,'')
 
-    # 抽出した値を一つにまとめて、timeオブジェクト経由でunixtimeにする$
-    time = "#{tmp_date} #{tmp_time}"
-    time = Time.strptime(time, @time_format).to_i
+      # 抽出した値を一つにまとめて、timeオブジェクト経由でunixtimeにする$
+      time = "#{tmp_date} #{tmp_time}"
+      time = Time.strptime(time, @time_format).to_i
 
-    # 処理が終了したので、配列から先頭2つを削除$
-    elements.shift(2)
+      # 処理が終了したので、配列から先頭2つを削除$
+      elements.shift(2)
 
-    # トラフィックログの処理
-    record = {}
-    while k = elements.shift
-      k =~ /(.+)=(.+)/
-      record[$1] = $2
+      # トラフィックログの処理
+      record = {}
+      while k = elements.shift
+        k =~ /(.+)=(.+)/
+        record[$1] = $2
+      end
+
+      return time, record
+
     end
-
-    return time, record
   end
 end
